0dc73f19c49783c40d260d28ccf1397488e52341
He Liu <liulonnie@gmail.com>
2019-12-06 23:46:04 -0800

    fix etcd import path

--- registry/etcd/etcd.go
+++ registry/etcd/etcd.go
@@ -13,11 +13,11 @@ import (
 	"sync"
 	"time"
 
-	"github.com/coreos/etcd/clientv3"
-	"github.com/coreos/etcd/etcdserver/api/v3rpc/rpctypes"
 	"github.com/micro/go-micro/registry"
 	"github.com/micro/go-micro/util/log"
 	hash "github.com/mitchellh/hashstructure"
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/etcdserver/api/v3rpc/rpctypes"
 )
 
 var (
